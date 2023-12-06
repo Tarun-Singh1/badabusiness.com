c80e323108c2e76f41cb490ed0086949f29bd748
Tarun Singh <143503988+Tarun-Singh1@users.noreply.github.com>
2023-12-06 20:12:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitForm() {
+  // Your form submission logic goes here
+  
+  // Show alert to the user
+  alert("You have successfully registered. You will be contacted by us soon.");
+}
+
